c29b38c0d21b504a3e704e5ca56a885d7fa95068
Sanyam Agarwal <36538899+SanyamAgarwalRobotics@users.noreply.github.com>
2019-02-18 21:07:18 +0530

    Update sim_model.py

--- ros/src/tl_detector/light_classification/sim_model.py
+++ ros/src/tl_detector/light_classification/sim_model.py
@@ -6,24 +6,24 @@ class SimModel():
   def __init__(self):
     self.GREEN_CHANNEL = 1
     self.RED_CHANNEL = 2
-    self.area_thr = 80
+    self.area_thrld = 79
   def predict(self, image):
     """
     image: cv2.Image (BGR)
     """
-    red_img = image[:,:,self.RED_CHANNEL]
-    green_img = image[:,:,self.GREEN_CHANNEL]
-    red_area = np.sum(red_img == red_img.max())
-    green_area = np.sum(green_img == green_img.max())
+    red_imge = image[:,:,self.RED_CHANNEL]
+    green_imge = image[:,:,self.GREEN_CHANNEL]
+    red_area = np.sum(red_imge == red_imge.max())
+    green_area = np.sum(green_imge == green_imge.max())
 
 
     prediction = TrafficLight.UNKNOWN
 
-    if red_area >= self.area_thr and green_area <= self.area_thr:
+    if red_area >= self.area_thrld and green_area <= self.area_thrld:
       prediction = TrafficLight.RED
-    elif red_area >= self.area_thr and green_area >= self.area_thr:
+    elif red_area >= self.area_thrld and green_area >= self.area_thrld:
       prediction = TrafficLight.YELLOW if 0.8 <= red_area / green_area <= 1.2 else TrafficLight.RED
-    elif green_area >= self.area_thr:
+    elif green_area >= self.area_thrld:
       prediction = TrafficLight.GREEN
     else:
       prediction = TrafficLight.UNKNOWN
